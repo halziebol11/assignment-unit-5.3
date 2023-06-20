f5a7dae55e664d56f81462c0739ba55bd9a9d27e
Haleigh Ziebol <halziebol11@gmail.com>
2023-06-20 13:03:04 -0500

    created addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,2 +1,15 @@
 console.log('***** Music Collection *****')
 let collection = [];
+
+function addToCollection(title, artist, yearPublished){
+    let Object = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished,
+    }
+    collection.push(Object);
+    return Object
+}
+
+addToCollection('Happy Bops', 'Bopper', 1990);
+console.log(collection)
