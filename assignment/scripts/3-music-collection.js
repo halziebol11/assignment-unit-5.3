6b01b4353c02ebafff100bd70078d1829a8c6c78
Haleigh Ziebol <halziebol11@gmail.com>
2023-06-20 13:08:00 -0500

    tested addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -9,7 +9,13 @@ function addToCollection(title, artist, yearPublished){
     }
     collection.push(Object);
     return Object
-}
+}//end of addToCollection
 
-addToCollection('Happy Bops', 'Bopper', 1990);
+//test addToCollection function
+console.log(addToCollection('Happy Bops', 'Bopper', 1990));
+console.log(addToCollection('Bippity Boppity', 'Bopper', 1991));
+console.log(addToCollection('Boogy Woogy', 'Bopper', 1992));
+console.log(addToCollection('Sad Bops for Sad Boppers', 'Bopper', 1993));
+console.log(addToCollection('R We Boppin?', 'Bopper', 1994));
+console.log(addToCollection('Cats R Sick', 'Cat Dudes', 1989));
 console.log(collection)
